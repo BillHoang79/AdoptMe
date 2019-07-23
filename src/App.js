3c519a2fa85ba6dcd2915bdac39b666d43ec3743
BillHoang79 <billhoang79@gmail.com>
2019-07-23 00:24:30 -0700

    setting state with useState Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Cat" breed="Mixed" />
-      <Pet name="Pepper" animal="Dog" breed="Havanese" />
-      <Pet name="Goldy" animal="Fish" breed="Goldfish" />
+      <SearchParams />
     </div>
   );
 };
